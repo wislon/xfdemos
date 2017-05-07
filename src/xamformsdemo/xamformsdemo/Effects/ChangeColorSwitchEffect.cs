9bf8e202ab6841ad4b2dfb480cbf8a8db5e9d7e3
John Wilson <wislon@gmail.com>
2017-05-07 11:50:17 +1000

    add a generic ChangeColorEffect for toggle-able controls. then add ChangeColorSwitchEffect inheriting from it.

---
+++ src/xamformsdemo/xamformsdemo/Effects/ChangeColorSwitchEffect.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace xamformsdemo.Effects
+{
+  public class ChangeColorSwitchEffect : RoutingEffect
+  {
+    /// <summary>
+    /// [assembly: ResolutionGroupName("xamformsdemo")] moved to the AssemblyInfo
+    /// classes in the platform projects
+    /// </summary>
+    public ChangeColorSwitchEffect() : base("xamformsdemo.ChangeColorSwitchEffect")
+    {
+    }
+  }
+}
