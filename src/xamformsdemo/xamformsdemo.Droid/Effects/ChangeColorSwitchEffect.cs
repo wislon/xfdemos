a7bae5f8734abd884174e16de071f505f3fd64fd
John Wilson <wislon@gmail.com>
2017-05-07 15:55:24 +1000

    tweak droid ChangeColorSwitchEffect so it uses ThumbColorProperty
    - with commented code to change thumb colour to true/false color if you want.

--- src/xamformsdemo/xamformsdemo.Droid/Effects/ChangeColorSwitchEffect.cs
+++ src/xamformsdemo/xamformsdemo.Droid/Effects/ChangeColorSwitchEffect.cs
@@ -1,13 +1,13 @@
 ﻿using Android.Graphics;
 using Android.Support.V7.Widget;
 using Android.Widget;
-using xamformsdemo.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Color = Xamarin.Forms.Color;
 using Switch = Android.Widget.Switch;
 
 using ChangeColorSwitchEffect = xamformsdemo.Droid.Effects.ChangeColorSwitchEffect;
+using XECCSwitchEffect = xamformsdemo.Effects.ChangeColorSwitchEffect;
 
 [assembly: ExportEffect(typeof(ChangeColorSwitchEffect), "ChangeColorSwitchEffect")]
 
@@ -22,6 +22,7 @@ namespace xamformsdemo.Droid.Effects
   {
     private Color _trueColor;
     private Color _falseColor;
+    private Color _thumbColor;
 
     // slightly darker for the tracks, otherwise there's no virtual
     // 'depth' below the 'thumb' part of the slider.
@@ -32,16 +33,20 @@ namespace xamformsdemo.Droid.Effects
     {
       if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.JellyBean)
       {
-        _trueColor = (Color)Element.GetValue(ChangeColorEffect.TrueColorProperty);
-        _trueColorDarker = _trueColor.AddLuminosity(-0.25);
+        _thumbColor = (Color)Element.GetValue(XECCSwitchEffect.ThumbColorProperty);
+        _trueColor = (Color)Element.GetValue(XECCSwitchEffect.TrueColorProperty);
+        _falseColor = (Color)Element.GetValue(XECCSwitchEffect.FalseColorProperty);
 
-        _falseColor = (Color)Element.GetValue(ChangeColorEffect.FalseColorProperty);
         _falseColorDarker = _falseColor.AddLuminosity(-0.25);
+        _trueColorDarker = _trueColor.AddLuminosity(-0.25);
 
         ((SwitchCompat)Control).CheckedChange += OnCheckedChange;
 
-        ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_falseColor.ToAndroid(), PorterDuff.Mode.Multiply);
         ((SwitchCompat)Control).TrackDrawable.SetColorFilter(_falseColorDarker.ToAndroid(), PorterDuff.Mode.Multiply);
+
+        ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_thumbColor.ToAndroid(), PorterDuff.Mode.Multiply);
+        // to change the colour of the thumb-drawable to the 'true' (or 'false') colour, enable the line below (and disable the one above)
+        // ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_trueColor.ToAndroid(), PorterDuff.Mode.Multiply);
       }
     }
 
@@ -49,13 +54,15 @@ namespace xamformsdemo.Droid.Effects
     {
       if (checkedChangeEventArgs.IsChecked)
       {
-        ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_trueColor.ToAndroid(), PorterDuff.Mode.Multiply);
         ((SwitchCompat)Control).TrackDrawable.SetColorFilter(_trueColorDarker.ToAndroid(), PorterDuff.Mode.Multiply);
+        // to change the colour of the thumb-drawable to the 'true' (or false) colour, enable the line below
+        // ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_trueColor.ToAndroid(), PorterDuff.Mode.Multiply);
       }
       else
       {
-        ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_falseColor.ToAndroid(), PorterDuff.Mode.Multiply);
         ((SwitchCompat)Control).TrackDrawable.SetColorFilter(_falseColorDarker.ToAndroid(), PorterDuff.Mode.Multiply);
+        // to change the colour of the thumb-drawable to the 'true' (or false) colour, enable the line below
+        // ((SwitchCompat)Control).ThumbDrawable.SetColorFilter(_thumbColor.ToAndroid(), PorterDuff.Mode.Multiply);
       }
     }
 
