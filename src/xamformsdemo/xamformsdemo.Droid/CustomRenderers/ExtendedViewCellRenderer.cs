f2d6ec8662923d13167ccc04c69d75b83159909d
John Wilson <wislon@gmail.com>
2018-05-01 08:33:59 +1000

    add todos following more research on recycled views for next steps

--- src/xamformsdemo/xamformsdemo.Droid/CustomRenderers/ExtendedViewCellRenderer.cs
+++ src/xamformsdemo/xamformsdemo.Droid/CustomRenderers/ExtendedViewCellRenderer.cs
@@ -12,33 +12,38 @@ using xamformsdemo.Droid.CustomRenderers;
 [assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
 namespace xamformsdemo.Droid.CustomRenderers
 {
-  public class ExtendedViewCellRenderer : ViewCellRenderer
+  /// <summary>
+  /// TODO it looks like we need to have a different renderer for Recycle-based views. Since the native views
+  /// TODO are recycled, they can't track their own 'selected' properties (or you'll see more than one cell becoming
+  /// TODO selected as you scroll back and forth). We'll need the XF 'ExtendedViewCell' to keep track of its
+  /// TODO own 'selected' state (via ItemSelected event?) and then feed that into here, via GetCellCore and 
+  /// TODO probably the BindingContextChanged property for OnCellPropertyChanged below (via 'sender').
+  /// TODO Ppl wonder why Xamarin hasn't done this for us... I think  this explains it, it's too %$#%$ hard to do, because it's 
+  /// TODO bespoke for every custom view for every list for every app.
+  /// TODO Next steps: 
+  /// TODO 0. Revert Everything here (to where we branched) and check that the RetainElement still works as expected.
+  /// TODO 1. Create a different ExtendedRecycledViewCellRenderer (or something), specifically for the Recycle-type implementation.
+  /// TODO 2. Add another demo button to showcase Recycle-type implementation
+  /// </summary>
+  public class ExtendedViewCellRenderer : ViewCellRenderer, INativeElementView
   {
     private readonly Android.Graphics.Color _defaultBackgroundColor = Android.Graphics.Color.Transparent;
 
     private Android.Views.View _cellCore;
     private Drawable _unselectedBackground;
     private bool _selected;
+    public Element Element { get; private set; }
 
     protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
     {
-      var extendedViewCell = (ExtendedViewCell)item;
+      Element = (ExtendedViewCell)item;
      
       _cellCore = base.GetCellCore(item, convertView, parent, context);
-
-      TODO  remember to unsubscribe from this
-      _cellCore.Click += (sender, args) =>
-      {
-        TODO do we really need to track the selected status in the xplat view??
-        extendedViewCell.IsSelected = !extendedViewCell.IsSelected;
-        _selected = extendedViewCell.IsSelected;
-      };
 
       // Save/set original background to roll-back to it when not selected,
       // we're assuming that no cells will be selected on creation.
       //_selected = extendedViewCell.IsSelected;
       _unselectedBackground = _cellCore.Background ?? new ColorDrawable(_defaultBackgroundColor);
-
 
       return _cellCore;
     }
@@ -53,7 +58,7 @@ namespace xamformsdemo.Droid.CustomRenderers
     {
       base.OnCellPropertyChanged(sender, args);
 
-      which IsSelected is showing, the one for the xamforms view, or the android native one?
+      //which IsSelected is showing, the one for the xamforms view, or the android native one?
       if (args.PropertyName == "IsSelected" || args.PropertyName=="Index")
       {
         // Had to create a property to track the selection because cellCore.Selected is always false.
@@ -70,6 +75,7 @@ namespace xamformsdemo.Droid.CustomRenderers
         }
       }
     }
+
   }
 
 }
