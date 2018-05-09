9d877fecb53ffe86f65e3786da0b5bc8c9f62680
John Wilson <wislon@gmail.com>
2018-05-09 21:26:52 +1000

    (wip) on ios ExtendedViewCellRenderer. Works for Retain, but annoying weirdness in Recycle

--- src/xamformsdemo/xamformsdemo.iOS/CustomRenderers/ExtendedViewCellRenderer.cs
+++ src/xamformsdemo/xamformsdemo.iOS/CustomRenderers/ExtendedViewCellRenderer.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
-using CoreGraphics;
 using UIKit;
 using xamformsdemo.CustomControls;
 using xamformsdemo.iOS.CustomRenderers;
-using xamformsdemo.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -17,16 +15,23 @@ namespace xamformsdemo.iOS.CustomRenderers
     private ExtendedViewCell _extendedViewCell;
     private UIColor _selectedBackgroundColor;
     private UITableViewCell _cell;
+    private UIColor _unselectedBackgroundColor;
 
     public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
     {
       _extendedViewCell = item as ExtendedViewCell;
-      _selectedBackgroundColor = _extendedViewCell.SelectedBackgroundColor.ToUIColor();
+      if (_extendedViewCell != null)
+      {
+        _selectedBackgroundColor = _extendedViewCell.SelectedBackgroundColor.ToUIColor();
+        _unselectedBackgroundColor = _extendedViewCell.View.BackgroundColor.ToUIColor();
+      }
 
       if (_extendedViewCell is BindableObject evcAsBindable)
       {
         // wire this up so 'CurrentlySelected' property changes are propagated to the handler below.
         evcAsBindable.PropertyChanged += ExtendedViewCellPropertyChanged;
+        _extendedViewCell.BindingContextChanged += ExtendedViewCellOnBindingContextChanged;
+
       }
 
       _cell = base.GetCell(item, reusableCell, tv);
@@ -39,15 +44,26 @@ namespace xamformsdemo.iOS.CustomRenderers
       return _cell;
     }
 
+    private void ExtendedViewCellOnBindingContextChanged(object sender, EventArgs eventArgs)
+    {
+      this.ExtendedViewCellPropertyChanged(_extendedViewCell, new PropertyChangedEventArgs("CurrentlySelected"));
+    }
+
     private void ExtendedViewCellPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
     {
       if (propertyChangedEventArgs.PropertyName == "CurrentlySelected")
       {
-        // this works for RetainElement, but not for RecycleElement (need another propertyname ?)
-        // animation shows what's happening, as cell becomes deselected, it animates from selected background colour to black,
-        // and selected cell becomes white again
+        // this works perfectly for RetainElement. 
+        // RecycleElement works too, EXCEPT that when a cell scrolls out of view and back in again, it comes back 
+        // selected in white, and the same element one 'screen length' away can be seen to quickly animate as cell 
+        // becomes deselected (from being already deselected, it's weird), it animates from black to selected
+        // background colour to black again really quickly. If animation wasn't enabled, you'd probably not even see it.
         _cell.SetSelected(_extendedViewCell.CurrentlySelected, true);
+        _cell.ContentView.BackgroundColor = _extendedViewCell.CurrentlySelected
+          ? _selectedBackgroundColor
+          : _unselectedBackgroundColor;
       }
+
     }
 
   }
